910a1513612f4203f8a0b24aede890e548e8ba1e
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-17 16:32:33 +0100

    Add Prisma + basic configurations

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { PrismaService } from './prisma/prisma.service';
+import { ValidationPipe } from '@nestjs/common';
+import { LoggerInterceptor } from './shared/interceptors/logger.interceptor';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  app.useGlobalPipes(new ValidationPipe({ transform: true }));
+  app.useGlobalInterceptors(new LoggerInterceptor());
+  app.setGlobalPrefix('api');
+
+  const prismaService = app.get(PrismaService);
+  await prismaService.enableShutdownHooks(app);
+
+  await app.listen(8000);
 }
-bootstrap();
