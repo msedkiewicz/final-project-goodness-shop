d424acdde3ed240ee200669691d14b06f53548ec
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-17 17:34:33 +0100

    Add additional configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,18 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ProductModule } from './product/product.module';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
-  imports: [PrismaModule, ProductModule],
+  imports: [
+    PrismaModule,
+    ProductModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'client', 'build'),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
