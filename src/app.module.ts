543b2dfe6bdebb92a096ee364d0b4e52b7ca5fce
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-17 18:47:58 +0100

    Add Auth files

--- src/app.module.ts
+++ src/app.module.ts
@@ -7,6 +7,7 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { ServeStaticModule } from '@nestjs/serve-static';
 import { join } from 'path';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -16,6 +17,7 @@ import { UsersModule } from './users/users.module';
       rootPath: join(__dirname, '..', 'client', 'build'),
     }),
     UsersModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
