96065539a01cb8c9b16557c7f8a72b7cbd369fc4
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-17 16:51:36 +0100

    Add GET endpoints for Product

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -1,7 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param, ParseUUIDPipe } from '@nestjs/common';
 import { ProductService } from './product.service';
 
 @Controller('product')
 export class ProductController {
   constructor(private productService: ProductService) {}
+
+  @Get('/')
+  getAll() {
+    return this.productService.getAll();
+  }
+
+  @Get('/:id')
+  async getById(@Param('id', new ParseUUIDPipe()) id: string) {
+    const product = await this.productService.getById(id);
+    if (!product) throw new Error('Product not found');
+    return product;
+  }
 }
--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -5,4 +5,14 @@ import { Product } from './product.interface';
 @Injectable()
 export class ProductService {
   constructor(private prismaService: PrismaService) {}
+
+  public async getAll(): Promise<Product[]> {
+    return this.prismaService.product.findMany();
+  }
+
+  public getById(id: Product['id']): Promise<Product | null> {
+    return this.prismaService.product.findUnique({
+      where: { id },
+    });
+  }
 }
