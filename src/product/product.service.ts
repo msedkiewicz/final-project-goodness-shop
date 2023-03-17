9dd423ded5aac58d558d11d99a29c95b8a434116
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-17 16:48:07 +0100

    Add Product interface

--- src/product/product.service.ts
+++ src/product/product.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@nestjs/common';
 import { PrismaService } from '../prisma/prisma.service';
+import { Product } from './product.interface';
 
 @Injectable()
 export class ProductService {
