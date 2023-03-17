636c8a8222fe7ecf49a162e6d5822e044174215b
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-17 20:04:05 +0100

    Add basic front site structure

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,18 @@
+import { Container } from 'react-bootstrap';
+import { Route, Routes } from 'react-router-dom';
+import Footer from './components/views/Footer/Footer';
+import Home from './components/pages/Home/Home';
+import Navbar from './components/views/Navbar/Navbar';
+
 function App() {
   return (
-    <div className="App">
-      <h1>Hello world!</h1>
-    </div>
+    <Container>
+      <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+      </Routes>
+      <Footer />
+    </Container>
   );
 }
 
