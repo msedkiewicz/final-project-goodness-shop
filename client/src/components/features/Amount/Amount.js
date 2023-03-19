fbab50c02bd19252ec248a938badc77afa3fa9d5
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-19 23:20:21 +0100

    Add Amount component

---
+++ client/src/components/features/Amount/Amount.js
@@ -0,0 +1,54 @@
+import React, { useEffect, useState } from 'react';
+import { Button } from 'react-bootstrap';
+import styles from './amount.module.scss';
+
+const Amount = (props) => {
+  const [value, setValue] = useState(1);
+
+  const addItem = () => {
+    if (value <= 9) {
+      setValue(value + 1);
+    }
+  };
+  const removeItem = () => {
+    if (value >= 2) {
+      setValue(value - 1);
+    }
+  };
+
+  useEffect(() => {
+    props.onClick(value);
+  }, [value]);
+
+  return (
+    <div className="d-flex flex-row">
+      <h4>Amount:</h4>
+      <div className={'d-flex ' + styles.amount}>
+        <Button
+          onClick={() => {
+            removeItem();
+          }}
+          variant="outline-dark"
+        >
+          -
+        </Button>
+        <input
+          type="text"
+          value={value}
+          readOnly
+          className={'text-center ' + styles.input}
+        />
+        <Button
+          onClick={() => {
+            addItem();
+          }}
+          variant="outline-dark"
+        >
+          +
+        </Button>
+      </div>
+    </div>
+  );
+};
+
+export default Amount;
