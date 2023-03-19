ee6d8c78fff16c9a4d67e21885a6047cb8dbdc15
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-19 22:08:18 +0100

    Add OfferContainer component

---
+++ client/src/components/features/OfferContainer/OfferContainer.js
@@ -0,0 +1,87 @@
+import React, { useEffect, useState } from 'react';
+import { Container, Spinner, Col, Row } from 'react-bootstrap';
+import { Tab, Tabs, TabList, TabPanel } from 'react-tabs';
+import { useDispatch, useSelector } from 'react-redux';
+import { API_URL } from '../../../config';
+import {
+  getProducts,
+  getProductsBasic,
+  getProductsExtended,
+  updateProducts,
+} from '../../../redux/productsRedux';
+import styles from './OfferContainer.module.scss';
+import OfferBox from '../OfferBox/OfferBox'
+
+const OfferContainer = () => {
+  const [isLoading, setLoading] = useState(false);
+  const [products, setProducts] = useState();
+  const dispatch = useDispatch();
+  useEffect(() => fetchData(), []);
+
+  function fetchData() {
+    setLoading(true);
+    fetch(API_URL + '/product')
+      .then((res) => res.json())
+      .then(async (products) => {
+        await dispatch(updateProducts(products));
+        setLoading(false);
+      });
+  }
+  const allProducts = useSelector(getProducts);
+  const productsMen = useSelector(getProductsBasic);
+  const productsWoman = useSelector(getProductsExtended);
+  if (isLoading) {
+    return (
+      <Spinner animation="border" role="status" className="d-block mx-auto">
+        <span className="visually-hidden">Loading...</span>
+      </Spinner>
+    );
+  } else
+    return (
+      <Row className={styles.root}>
+        <Tabs>
+          <Col className={'text-center mt-3'}>
+            <TabList className={'mt-5 ' + styles.tabList}>
+              <Tab className={styles.tab}>All</Tab>
+              <Tab className={styles.tab}>Extended</Tab>
+              <Tab className={styles.tab}>Basic</Tab>
+            </TabList>
+          </Col>
+          <Container className="mt-5">
+            <TabPanel>
+              {' '}
+              <Row xs={1} md={2} lg={4} className="g-3 ">
+                {allProducts.map((product) => (
+                  <Col key={product.id}>
+                    <OfferBox {...product} />
+                  </Col>
+                ))}
+              </Row>
+            </TabPanel>
+            <TabPanel>
+              {' '}
+              <Row xs={1} md={2} lg={4} className="g-3 ">
+                {productsWoman.map((product) => (
+                  <Col key={product.id}>
+                    <OfferBox {...product} />
+                  </Col>
+                ))}
+              </Row>
+            </TabPanel>
+            <TabPanel>
+              {' '}
+              <Row xs={1} md={2} lg={4} className="g-3 ">
+                {productsMen.map((product) => (
+                  <Col key={product.id}>
+                    <OfferBox {...product} />
+                  </Col>
+                ))}
+              </Row>
+            </TabPanel>
+          </Container>
+        </Tabs>
+      </Row>
+    );
+};
+
+export default OfferContainer;
--- client/src/components/pages/Home/Home.js
+++ client/src/components/pages/Home/Home.js
@@ -1,12 +1,14 @@
 import React from 'react';
 import CarouselHome from '../../features/Carousel/Carousel';
 import InformationBox from '../../features/InformationBox/InformationBox';
+import OfferContainer from '../../features/OfferContainer/OfferContainer';
 
 const Home = () => {
   return (
     <div>
       <CarouselHome />
       <InformationBox />
+      <OfferContainer />
     </div>
   );
 };
