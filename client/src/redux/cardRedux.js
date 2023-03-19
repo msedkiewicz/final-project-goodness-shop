346ba4158f684863891851e7449cfdd3b3c56fa9
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-19 21:12:01 +0100

    Add cardRedux code

--- client/src/redux/cardRedux.js
+++ client/src/redux/cardRedux.js
@@ -1,14 +1,22 @@
 //selectors
-
+export const getCart = ({ cart }) => cart;
+export const getCartById = ({ cart }, id) =>
+  cart.find((cart) => cart.id === id);
 // actions
 const createActionName = (actionName) => `app/products/${actionName}`;
-
+const ADD_CART = createActionName('ADD_CART');
 // action creators
+export const addCart = (payload) => ({ type: ADD_CART, payload });
 
-const cardReducer = (statePart = [], action) => {
+const cartReducer = (statePart = [], action) => {
   switch (action.type) {
+    case ADD_CART:
+      let index = statePart.findIndex((cart) => cart.id === action.payload.id);
+
+      if (index === -1) return [...statePart, action.payload];
+      return statePart;
     default:
       return statePart;
   }
 };
-export default cardReducer;
+export default cartReducer;
