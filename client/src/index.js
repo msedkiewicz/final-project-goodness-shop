95c4bca931ce45aeee80a0fe64916f0b0bd379c5
Magdalena Sedkiewicz <magdalenainspirations@gmail.com>
2023-03-19 17:08:20 +0100

    Add Redux to app

--- client/src/index.js
+++ client/src/index.js
@@ -2,14 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import { BrowserRouter } from 'react-router-dom';
-import "bootstrap/dist/css/bootstrap.min.css";
-import "./styles/global.scss";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/global.scss';
+import { Provider } from 'react-redux';
+import store from './redux/store';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <BrowserRouter>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </BrowserRouter>
-  </React.StrictMode>
+  </React.StrictMode>,
 );
